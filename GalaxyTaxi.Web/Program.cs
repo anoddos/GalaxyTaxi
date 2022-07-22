87c2f175be4f6378277f4057dbcd052023d8f7eb
Izzie Baghdavadze <izibaghdavadze@gmail.com>
2022-07-22 04:01:52 +0400

    Integrated MudBlazor. Created Simple login dialog

--- GalaxyTaxi.Web/Program.cs
+++ GalaxyTaxi.Web/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using GalaxyTaxi.Web;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(_ => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+builder.Services.AddMudServices();
+builder.Services.AddMudBlazorDialog();
+builder.Services.AddMudBlazorSnackbar();
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
